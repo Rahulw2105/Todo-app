f61d53660f5fdd376988b33a875ad176f40bd798
Rahulw2105 <wankhederahul116@gmail.com>
2025-01-17 01:36:58 +0530

    fetch data using axios to get all todos and setup fetch api in service.js

--- src/components/TableTask.jsx
+++ src/components/TableTask.jsx
@@ -1,6 +1,13 @@
-import React from "react";
-
+import React, { useEffect } from "react";
+import { getTodos } from "../services/service";
 const TableTask = () => {
+  useEffect(() => {
+    async function fetchData() {
+      const fetchedTodos = await getTodos();
+      console.log(fetchedTodos);
+    }
+    fetchData();
+  }, []);
   return <div>TableTask</div>;
 };
 
---
+++ src/services/service.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const TASK_URL = "http://localhost:3000/api/tasks";
+
+export const getTodos = async () => {
+  try {
+    const response = await axios.get(TASK_URL);
+    return response.data;
+  } catch (err) {
+    throw new Error(`Error while fetching todos: ${err.message}`);
+  }
+};
