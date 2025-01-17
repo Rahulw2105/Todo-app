574efc4fffde01119cdf33faba78c9d2924f5c46
Rahulw2105 <wankhederahul116@gmail.com>
2025-01-17 06:15:41 +0530

    post and put api for todos

--- src/services/service.js
+++ src/services/service.js
@@ -2,6 +2,25 @@ import axios from "axios";
 
 const TASK_URL = "http://localhost:3000/api/tasks";
 const DELETE_URL = "http://localhost:3000/api/task";
+
+// edit todo and create new todo
+export const saveTodos = async (task) => {
+  if (task.Id) {
+    try {
+      const response = await axios.put(`${TASK_URL}/${task.Id}`, task);
+      return response.data;
+    } catch (err) {
+      throw new Error(`Error while updating todos: ${err.message}`);
+    }
+  } else {
+    try {
+      const response = await axios.post(TASK_URL, task);
+      return response.data;
+    } catch (err) {
+      throw new Error(`Error while saving new todo: ${err.message}`);
+    }
+  }
+};
 // to get all the todos
 export const getTodos = async () => {
   try {
@@ -11,7 +30,7 @@ export const getTodos = async () => {
     throw new Error(`Error while fetching todos: ${err.message}`);
   }
 };
-//
+// delete todo
 export const deleteTodos = async (taskId) => {
   try {
     const response = await axios.delete(`${DELETE_URL}/${taskId}`);
